dbe85a6283ebd549e8d64fd1f19d36b1e26724df
david <leachda12@icloud.com>
2023-11-16 18:56:30 -0500

    made user_id for employees and patients nullable

--- database/migrations/2023_11_14_162302_create_employees_table.php
+++ database/migrations/2023_11_14_162302_create_employees_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('employees', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained();
+            $table->foreignId('user_id')->constrained()->nullable();
             $table->integer('salary')->default(20000);
             $table->timestamps();
         });
