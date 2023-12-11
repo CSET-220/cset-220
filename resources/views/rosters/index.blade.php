18537b09b34d8089c3e6f55f06c1e2b043f3052a
david <leachda12@icloud.com>
2023-12-11 11:03:27 -0500

    rosters index duplicate jquery CDN removed

--- resources/views/rosters/index.blade.php
+++ resources/views/rosters/index.blade.php
@@ -61,7 +61,6 @@
 @endsection
 
 @section('script')
-<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.js"></script>
 <script src={{asset('js/rosters/rosters-index.js')}}></script>
 @endsection
