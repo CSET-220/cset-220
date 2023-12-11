c6fa23e092ad026c87269a8fa670f35a9fbf0e1b
david <leachda12@icloud.com>
2023-12-11 10:59:02 -0500

    caregivers home duplicate jquery CDN removed

--- resources/views/caregivers/home.blade.php
+++ resources/views/caregivers/home.blade.php
@@ -115,7 +115,6 @@
 
 
 @section('script')
-<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.js"></script>
 <script src="{{asset('js/logs/logs-index.js')}}"></script>
 @endsection
