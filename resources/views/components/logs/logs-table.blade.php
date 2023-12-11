f5089f574c4ffadd0edb6249f7cca2bb491a3cd2
david <leachda12@icloud.com>
2023-12-11 11:00:02 -0500

    logs component duplicate jquery CDN removed

--- resources/views/components/logs/logs-table.blade.php
+++ resources/views/components/logs/logs-table.blade.php
@@ -78,7 +78,6 @@
 
 @section('script')
 
-    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
