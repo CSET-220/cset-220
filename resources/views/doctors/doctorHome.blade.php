1e1aa00c26a7f57bb49aa1dee8a843d2702c101a
david <leachda12@icloud.com>
2023-12-11 11:01:00 -0500

    doctor home removed commented out CDN

--- resources/views/doctors/doctorHome.blade.php
+++ resources/views/doctors/doctorHome.blade.php
@@ -183,7 +183,6 @@
 @endsection
 
 @section('script')
-{{-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>   --}}
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.10.21/js/dataTables.bootstrap4.min.js"></script>
